92c2603395036b1ab8b11bf01e3d496670f2be69
Jose Junior <jlf.junior@outlook.com>
2020-09-24 19:54:36 -0300

    Add entity BankAccount

---
+++ src/ChallengeBank.Domain/Entities/BankAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChallengeBank.Domain.Entities
+{
+    public class BankAccount
+    {
+        public long Id { get; set; }
+        public string BankBranch { get; set; }
+        public string AccountNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
