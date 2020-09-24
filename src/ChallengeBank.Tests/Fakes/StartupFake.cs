bf9452ce7afc7476427a053db1b582e96d5e864f
Jose Junior <jlf.junior@outlook.com>
2020-09-24 20:00:15 -0300

    Initial project of tests

---
+++ src/ChallengeBank.Tests/Fakes/StartupFake.cs
@@ -0,0 +1,9 @@
+﻿using ChallengeBank.Api;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ChallengeBank.Tests.Fakes
+{
+    public class StartupFake : WebApplicationFactory<Startup>
+    {
+    }
+}
---
+++ src/ChallengeBank.Tests/Integration/BankAccountControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using ChallengeBank.Tests.Fakes;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ChallengeBank.Tests.Integration
+{
+    public class BankAccountControllerTest : IClassFixture<StartupFake>
+    {
+        private readonly StartupFake _startup;
+
+        public BankAccountControllerTest(StartupFake startup)
+        {
+            _startup = startup;
+        }
+
+        [Fact]
+        public async Task ShouldCreate()
+        {
+
+        }
+    }
+}
